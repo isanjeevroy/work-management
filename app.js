33e8f0b9692c0160ce43a3edae5d7056e2a34d0e
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-03-17 02:12:35 +0530

    updated

--- app.js
+++ app.js
@@ -10,17 +10,20 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const session = require("express-session");
 const Job = require("./models/job.js");
+const User = require("./models/user.js");
 const WrapAsync = require("./utils/WrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const flash = require("connect-flash");
-const PDFDocument = require('pdfkit');
+const MongoStore = require('connect-mongo');
 const fs = require('fs');
+const passport = require("passport");
+const localStrategy = require("passport-local");
 const puppeteer = require('puppeteer');
 
 
 // Databases connection
 // const MONGO_URL = "mongodb://127.0.0.1:27017/sujeetwork";
-const dbUrl = process.env.ATLAS_URL;
+const MONGO_URL = process.env.ATLAS_URL;
 
 main().then(() => {
     console.log("Connected to DB");
@@ -29,7 +32,7 @@ main().then(() => {
 });
 
 async function main() {
-await mongoose.connect(dbUrl);
+await mongoose.connect(MONGO_URL);
 }
 
 // middlewares
@@ -41,8 +44,20 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, '/public')));
 
 
+
+const store = MongoStore.create({
+    mongoUrl: MONGO_URL,
+    crypto:{
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24* 3600,
+});
+
+store.on("error",()=>{
+    console.log("ERROR IN MONGO SESSION STORE",err);
+})
 const sessionOptions ={
-    secret:"rajivchutiya",
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     cookie:{
@@ -51,88 +66,125 @@ const sessionOptions ={
         httpOnly: true, 
     },
 };
-
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
+    res.locals.currUser=req.user;
     next();
 });
 
-// print data
-app.get("/print/:jobId", async(req, res) => {
-
-  const jobId = req.params.jobId;
-  const data = await Job.findById(jobId);
-  if (!data) {
-    req.flash("error","Job not found");
-    return res.status(404).redirect("/"); // Redirect to home page or an error page
-  }
-
-  const doc = new PDFDocument();
-
-  // Get current date and time
-  const currentDate = new Date().toLocaleString(); // Get current date and time
-  
-  // Print current date and time at top right side
-  doc.text(`Date and Time: ${currentDate}`, { align: 'right', width: 300 });
-
-  // Set up PDF content
-  doc.moveDown(2).fontSize(18).text(data.job_name, { align: 'center' }).moveDown(1);
-  
-  // Arrays to store completed and pending works
-  const completedWorks = [];
-  const pendingWorks = [];
-
-  // Separate completed and pending works
-  data.works.forEach(work => {
-    if (work.status) {
-      completedWorks.push(work.name);
-    } else {
-      pendingWorks.push(work.name);
-    }
-  });
-
-  // Function to calculate padding for numerical numbering
-  function calculatePadding(num) {
-    const maxLength = completedWorks.length + pendingWorks.length;
-    return (maxLength.toString().length - num.toString().length) + 1;
-  }
-
-  // Print completed works
-  doc.moveDown().fontSize(15).fillColor('green').text('Completed Status:');
-  completedWorks.forEach((work, index) => {
-    const padding = calculatePadding(index + 1);
-    doc.moveDown().fontSize(12).fillColor('black').text(`${index + 1}.${' '.repeat(padding)}${work}`);
-  });
-
-  // Print pending works
-  doc.moveDown().fontSize(15).fillColor('red').text('Pending Status:');
-  pendingWorks.forEach((work, index) => {
-    const padding = calculatePadding(index + 1);
-    doc.moveDown().fontSize(12).fillColor('black').text(`${index + 1}.${' '.repeat(padding)}${work}`);
-  });
-
-  // Finalize PDF
-  doc.end();
-
-  // Set response headers for PDF download
-  res.setHeader('Content-Disposition', 'attachment; filename="output.pdf"');
-  res.setHeader('Content-Type', 'application/pdf');
-
-  // Pipe the PDF document to the response stream
-  doc.pipe(res);
+// Registration
+app.get("/signup",async(req,res)=>{
+    res.render("users/signup.ejs");
 });
 
+// app.post("/signup",async(req,res)=>{
+//     try{
+//         let {username,email,password}= req.body;
+//         const newUser = new User({email,username});
+//         const registerdUser = await User.register(newUser,password);
+//         console.log(registerdUser);
+
+//             req.login(registerdUser,(err)=>{
+//                 if(err){
+//                     return next(err);
+//                 }
+//                 req.flash("success","Welcome to Wanderlust");
+//                 res.redirect("/data");
+//             }); 
+//     }catch(e){ 
+//         req.flash("error",e.message);
+//         res.redirect("/signup");
+//     }
+// });
+
+// Login
+app.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+})
+
+app.post("/login",
+passport.authenticate("local",{
+    failureRedirect:'/login',
+     failureFlash: true
+    }),
+    async(req,res)=>{
+    req.flash("success","Welcome back to page!");
+    res.redirect("/data");
+}
+);
 
 
+//Logout
+app.get("/logout",(req,res)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","you are logged out!");
+        res.redirect("/login");
+    });
+});
+
+//  PRINTING TABLE------------------------
+
+// print data
+app.get("/:jobId/print-data", async (req, res) => {
+    const jobId = req.params.jobId;
+    const data = await Job.findById(jobId);
+    res.render("print-data.ejs",{data});
+  
+});
+// print Route - Button
+app.get("/print/:jobId", async (req, res) => {
+    const jobId = req.params.jobId;
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged in to print");
+        return res.redirect("/login");
+    }
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`${req.protocol}://${req.get('host')}/${jobId}/print-data`, {
+            waitUntil: "networkidle2"
+        });
+        await page.setViewport({ width: 2080, height: 1050 });
+        const todayDate = new Date();
+        const pdfPath = path.join(__dirname, 'public/files', `${todayDate.getTime()}-${jobId}.pdf`);
+        await page.pdf({
+            printBackground:true,
+            path: pdfPath,
+            format: "A4"
+        });
+        await browser.close(); 
+        // res.sendFile(pdfPath, { headers: { "Content-Type": "application/pdf" } });
+        res.download(pdfPath,function(err){
+            if(err){
+                console.log(err);
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error generating PDF");
+    }
+});
 
 
 // Index Route
 app.get("/data",WrapAsync(async(req,res)=>{
-   
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged to access!");
+        return res.redirect("/login");
+    }
     const perPage = 1; 
     const page = parseInt(req.query.page) || 1;
     const allData = await Job.find({});
@@ -152,11 +204,19 @@ app.get("/data",WrapAsync(async(req,res)=>{
 
 // New Job Form Route
 app.get("/newjob",(req,res)=>{
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged to create new job!");
+        return res.redirect("/login");
+    }
     res.render("features/new-job.ejs");
 })
 
 //  Create new Job
 app.post("/newjob",WrapAsync(async(req,res)=>{
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged in to create new job!");
+        return res.redirect("/login");
+    }
     const {job_name} = req.body;
         const firstWork = await Job.findOne();
         const newJob = new Job({
@@ -194,21 +254,29 @@ app.post("/newjob",WrapAsync(async(req,res)=>{
 // }));
 
 app.get("/delete/:jobId", WrapAsync(async(req, res) => {
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged in to delete job!");
+        return res.redirect("/login");
+    }
   const previousUrl1 = req.session.urlHistory && req.session.urlHistory.length >= 2 ? req.session.urlHistory[req.session.urlHistory.length - 2] : "/";
     const previousUrl2 = req.session.urlHistory && req.session.urlHistory.length >= 3 ? req.session.urlHistory[req.session.urlHistory.length - 3] : "/";
 
   const jobId = req.params.jobId;
-  const alertMessage = "Are you sure you want to delete this job?";
+  const alertMessage = "Are you sure you want to delete the job?";
   res.render('deleteConfirmation', { alertMessage,jobId });
 }));
 
 
 app.get("/confirm-delete/:jobId", WrapAsync(async(req, res) => {
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged in to delete the job!");
+        return res.redirect("/login");
+    }
   const jobId = req.params.jobId;
 
   const deletedJob = await Job.findByIdAndDelete(jobId);
   const previousUrl = req.get('Referer') || '/';
-  console.log(previousUrl);
+//   console.log(previousUrl);
   const { pathname, search } = new URL(previousUrl); // Parse the previous URL
   const params = new URLSearchParams(search); // Parse the query string
   const page = params.get('page'); // Get the value of the 'page' parameter
@@ -217,20 +285,12 @@ app.get("/confirm-delete/:jobId", WrapAsync(async(req, res) => {
   res.redirect(`/data?page=${pageNo}`);
 }));
 
-
-
-
-
-
-
-
-
-
-
-
-
 // new work 
 app.post("/job/:jobId", WrapAsync(async(req,res)=>{
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged in to create new work!");
+        return res.redirect("/login");
+    }
         const {name} = req.body;
         const { jobId } = req.params;
         const job = await Job.findById(jobId);
@@ -246,6 +306,10 @@ app.post("/job/:jobId", WrapAsync(async(req,res)=>{
 
 // Update work status
 app.get("/job/:jobId/work/:workId/update",WrapAsync(async(req,res)=>{
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged in to update work!");
+        return res.redirect("/login");
+    }
         // console.log(req.url);
         const { jobId, workId } = req.params;
         const job = await Job.findById(jobId);
@@ -262,6 +326,10 @@ app.get("/job/:jobId/work/:workId/update",WrapAsync(async(req,res)=>{
 
 // Route to delete work
 app.get("/job/:jobId/work/:workId/delete",WrapAsync(async(req, res) => {
+    if(!req.isAuthenticated()){
+        req.flash("error","you must be logged in to delete!");
+        return res.redirect("/login");
+    }
 
         const { jobId, workId } = req.params;
         const job = await Job.findById(jobId);
