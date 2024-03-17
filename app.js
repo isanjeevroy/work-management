af9eeb598b87f1058f6f5e8431de49333bd01ef1
Sanjeev Roy <sanjeev15062002@gmail.com>
2024-03-17 15:36:16 +0530

    updated

--- app.js
+++ app.js
@@ -48,13 +48,13 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 
 
-const store = MongoStore.create({
-    mongoUrl: MONGO_URL,
-    crypto:{
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24* 3600,
-});
+// const store = MongoStore.create({
+//     mongoUrl: MONGO_URL,
+//     crypto:{
+//         secret: process.env.SECRET,
+//     },
+//     touchAfter: 24* 3600,
+// });
 
 store.on("error",()=>{
     console.log("ERROR IN MONGO SESSION STORE",err);
