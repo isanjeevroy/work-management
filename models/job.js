d8c65fa54019ab8931582a6a00a250b68b46f215
Sanjeev Kumar <sanjeev15062002@gmail.com>
2025-09-02 18:38:03 +0530

    linked job and user model

--- models/job.js
+++ models/job.js
@@ -2,17 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const jobSchema = new Schema({
-    job_name: {
-      type: String,
-      required: true
-    },
-    works: [
-      {
-        name: String,
-        status: Boolean
-      }
-    ]
-  });
+  job_name: {
+    type: String,
+    required: true
+  },
+  works: [
+    {
+      name: String,
+      status: Boolean
+    }
+  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+});
 
 module.exports = mongoose.model('Job', jobSchema);
-  
+
